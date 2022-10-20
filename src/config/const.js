e441b4c893164e5890147db766d1f70ae62ce67a
YoonkyungJung <yoonkyung0309@gmail.com>
2022-10-20 09:45:14 +0900

    API url 변경(Vercel)

--- src/config/const.js
+++ src/config/const.js
@@ -1,2 +1,4 @@
-export const API_URL = "http://localhost:8080";
-//export const API_URL = process.env.NODE_ENV === "production" ? "클라우드 서버 주소" : "http://localhost:8080";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://evo-museum-server.herokuapp.com"
+    : "http://localhost:8080";
